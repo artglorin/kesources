4fcfa9bef30481a385af2dbcb2edac20246ff39c
VERMINSKY Vladislav <Vladislav.VERMINSKY@raiffeisen.ru>
2021-03-26 03:14:46 +0300

    Update logging in design method

--- src/main/kotlin/arrtglorin/kubernetes/kesource/Styler.kt
+++ src/main/kotlin/arrtglorin/kubernetes/kesource/Styler.kt
@@ -1,3 +1,5 @@
+@file:Suppress("UnsafeCastFromDynamic")
+
 package arrtglorin.kubernetes.kesource
 
 import io.kvision.core.Container
@@ -6,11 +8,11 @@ import io.kvision.core.Container
 val designData = io.kvision.require("design/design.json")
 
 fun Container.design(designName: String) {
-    println("DESIGN $designName")
-    console.log("data", designData)
+    console.log("apply design = %", designName)
     setAttribute("design", (getAttribute("design")?.plus(", ")?:"") + designName)
-    val classes = designName.split('.')
-    val styles = classes.fold(designData) { acc, name -> if (acc == null) null else acc[name] }
+    val styles = designName
+        .split('.')
+        .fold(designData) { acc, name -> if (acc == null) null else acc[name] }
     if (styles == null) {
         console.error("Cannot load design by name '$designName'.", designData)
     } else {
